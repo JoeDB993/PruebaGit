92b4d4f274829d3d4b65bca077361ac5780009da
JoeDB993 <131511247+JoeDB993@users.noreply.github.com>
2024-09-10 18:34:40 -0600

    Agregar archivos de proyecto.

---
+++ PruebaGit/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PruebaGit
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PruebaGit/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PruebaGit
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
